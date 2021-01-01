02f8b4a5df7e7768f8245858d4eb88ca1be5642c
Hikaru Inomoto <medalhkr@gmail.com>
2021-01-01 20:37:01 +0900

    add config

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+cd $(dirname $0)
+
 sudo apt-get update
-sudo apt-get install -y direnv fish git jq neovim
+sudo apt-get install -y colordiff direnv fish git jq neovim peco tig
+
+if [ -d $HOME/.config ]; rm -r $HOME/.config; fi # clean for devcontainer
+ln -s $PWD/home/.config $HOME/.config
