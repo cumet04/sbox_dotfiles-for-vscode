5c4750a2177b916186c8d81f54df75df67c33869
Hikaru Inomoto <medalhkr@gmail.com>
2021-01-02 13:53:30 +0900

    wsl base
    これでdevcontainerで実行されないことが確認できればok

--- install.sh
+++ install.sh
@@ -8,3 +8,17 @@ sudo apt-get install -y colordiff direnv fish git jq neovim peco tig
 mkdir -p $HOME/.config
 CONF_ROOT=$(pwd -P)/home/.config
 ls -1 $CONF_ROOT | xargs -ISRC ln -s $CONF_ROOT/SRC $HOME/.config/
+
+
+### WSL setup
+test -z "$WSLENV" && exit
+
+sudo env DEBIAN_FRONTEND=noninteractive apt-get install -y ansible
+
+sudo ln -s (wslpath (wslvar USERPROFILE)) /opt/winhome # install windows home
+
+BRANCH=${BRANCH:-master}
+git clone -b $BRANCH https://github.com/cumet04/dotfiles $HOME/dotfiles
+
+cd $HOME/dotfiles/playbook
+ansible-playbook -K -c local -i localhost, entry.yaml
